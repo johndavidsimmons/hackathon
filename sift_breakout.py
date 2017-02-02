a5034f2d3d89bd74754875862f1dfd8faf5a1a3e
= <=>
2017-02-02 17:16:53 -0500

    Got the parsing working

--- sift_breakout.py
+++ sift_breakout.py
@@ -1,2 +1,16 @@
 import json
 import pandas as pd
+
+data = json.loads(open("C:\Temp\wewlad.json").read())
+
+skills = []
+interests = []
+for tm in data['results']:
+    try:
+        name = tm['firstName'] + ' ' + tm['lastName']
+        for skill in tm['skills']:
+            skills.append((name, skill.lower()))
+        for interest in tm['interests']:
+            interests.append((name, interest.lower()))
+    except:
+        pass
