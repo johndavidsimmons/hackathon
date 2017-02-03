1c0a5ac1b08d763ad4fdb7276ec85a4ad8e09319
Tyler Zupan <jtzupan@gmail.com>
2017-02-02 20:56:06 -0500

    added pages for adding questions and adding responders

--- app/main/views.py
+++ app/main/views.py
@@ -4,23 +4,67 @@ from . import main
 from flask_security import login_required, current_user
 from flask import request
 from .. import databaseModels as dm
-from forms import surveyForm
+from forms import surveyForm, respondersForm, questionForm
+import datetime
+
 
 @main.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@main.route('/createSurvey/')
+
+@main.route('/createSurvey/', methods=['GET', 'POST'])
 def createSurvey():
 
     # if user is submitting form
     if request.form:
         surveyName = request.form.get('surveyName')
-        b = dm.surveyModel(surveyName)
-        dm.db_session.add(b)
-        dm.db_session.commit()
-        return redirect(url_for('responders'))
+        surveyCreateDt = datetime.datetime.now()
+        responseLimit = request.form.get('responseLimit')
+        creatorID = ''
+        if request.form.get('publicSurvey') == 'Yes':
+            public = True
+        else:
+            public = False
 
-    #if users has not yet submitted form
+        b = dm.surveyModel(surveyName=surveyName,
+                           surveyCreateDt=surveyCreateDt,
+                           responseLimit=responseLimit,
+                           creatorID=creatorID,
+                           publicSurvey=public)
+        dm.db_session.add(b)
+        # dm.db_session.commit()
+        return redirect(url_for('main.addResponders'))
+
+    # if users has not yet submitted form
     form = surveyForm()
     return render_template('surveyName.html', form=form)
+
+
+@main.route('/addResponders/', methods=['GET', 'POST'])
+def addResponders():
+    if request.form:
+        responders = request.form.get("responders")
+        # b = dm.surveyModel()
+        # dm.db_session.add(b)
+        # dm.db_session.commit()
+        return redirect(url_for('main.addQuestion'))
+    form = respondersForm()
+    return render_template('addResponders.html', form=form)
+
+
+@main.route('/addQuestion/', methods=['GET', 'POST'])
+def addQuestion():
+    if request.form:
+        questionText = request.form.get("questionText")
+        # b = dm.questionModel()
+        # dm.db_session.add(b)
+        # dm.db_session.commit()
+        return redirect(url_for('main.Answers'))
+    form = questionForm()
+    return render_template('addQuestions.html', form=form)
+
+
+
+
+
